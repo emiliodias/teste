30b535334bc4d0139a2739a78f5002704a06fe92
emiliodias <emiliodias@gmail.com>
2013-03-31 23:19:04 -0300

    Update Servidor.java

--- Sockets/src/Servidor.java
+++ Sockets/src/Servidor.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 public class Servidor {
 
 	public static void main(String[] args) throws InterruptedException {
-		// declarando o servidor
+		// declarando o servidoraaaaaaaaaaaaaaaaa
 		ServerSocket serv = null;
 		// declarando o socket de comunica��o
 		Socket s = null;
